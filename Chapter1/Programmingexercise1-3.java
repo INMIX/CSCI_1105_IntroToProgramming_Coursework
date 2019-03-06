bc618ea49a8e195e26a0d16e08f87e9b94f2442f
404kevnotfound <student@CSiMac1-13.local>
2019-03-06 14:19:15 -0700

    added chapter 1

---
+++ Chapter1/Programmingexercise1-3.java
@@ -0,0 +1,6 @@
+public class ComputeExpression {
+	public static void main(string[ ] args) {
+		// Compute the equation located in the module
+		System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+	}
+}
---
+++ Chapter1/Programmingexercise1-9.java
@@ -0,0 +1,8 @@
+public class Area {
+
+	public static void main(String[] args) {
+		// Compute the area of an object
+		System.out.println("Area =");
+		System.out.println(4.5 * 7.9);
+	}
+}
